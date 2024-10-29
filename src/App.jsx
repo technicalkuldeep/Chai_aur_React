ce674128745688b7a977cca457f6c6260aa0361d
Kuldeep <kuldeep06102004@gmail.com>
2024-10-30 00:16:11 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import Mintu from "./Aryan";
+function App() {
+  
+  return (
+    <>
+      <h2> Kuldeep </h2>
+      <Mintu/>
+    </> 
+  )
+}
+
+export default App ;
---
+++ src/Aryan.jsx
@@ -0,0 +1,8 @@
+
+function Mintu () {
+    return (
+        <h3>Aryaan</h3>
+    )
+}
+
+export default Mintu ;
